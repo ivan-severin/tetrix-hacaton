8877eeb02692295cf4d85061224f4d9a3aebc7da
ivan-severin <ivan.severin.m@gmail.com>
2017-10-08 19:39:40 +0300

    lAST

--- src/web_ui/app.py
+++ src/web_ui/app.py
@@ -1,13 +1,143 @@
 from flask import Flask, render_template, request
 import json
-import communicate
+# from  communicate import *
 import pandas as pd
+
+import socket
+import threading
+# from app import data, colomns, index
+import os, time
+import threading
+
+import random
+
+
+
+
 
 """
 A example for creating a Table that is sortable by its header
 """
 
 app = Flask(__name__)
+
+
+data = []
+colomns =[]
+
+users = []
+current_user =  ''
+previous_user = '' 
+
+new_data = []
+
+
+def parse_recived_socket(dat):
+
+    dat =  dat.split(" ")
+    user = dat[0]
+
+    wines = [int(x) for x in dat[1:] ]
+    return user, wines
+
+def get_wine_data(ids , data, colomns):
+    new_dat =[]    
+    # ids = [ str(x) for x in ids]
+    print (ids)
+    print (colomns)
+    for j in range(1, len(ids)):
+        d = {} 
+        for i in range(1,len(colomns)):
+            key = colomns[i]["field"]
+            print(key)
+            d.update( { key : data[key][j] } )
+            
+    #         print(j, i)
+            print(d)
+            new_dat.append(d)
+
+    return new_dat
+
+
+
+class ThreadedServer(threading.Thread):
+
+    def __init__(self, host, port):
+        super(ThreadedServer, self).__init__()
+        
+        self.host = host
+        self.port = port
+        self.sock = socket.socket()
+        # self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        # self.clients = []
+        self.stoprequest = threading.Event()
+    
+    def run(self):
+        self.sock.listen(1)
+        while not self.stoprequest.isSet():
+            client, address = self.sock.accept()
+            self.listenToClient(client, address)
+            # client.settimeout(60)
+            # threading.Thread(target = self.listenToClient, args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        # self.clients.append(client)
+        while not self.stoprequest.isSet():
+            try:
+                msg= client.recv(size)
+                
+                
+
+                if msg:
+                    # Set the response to echo back the recieved data 
+                    print(msg)
+                    current_user, dat = parse_recived_socket(msg)
+                    # response = data
+                    if (current_user not in users):
+                        users.append(current_user)
+                        current_user
+                        dat
+                    else:
+                        if (previous_user != current_user):
+                           
+                            data
+                            # index(current_user)
+                            # render_template("table.html",
+                            #   data=data,
+                            #   columns=colomns,
+                            #   title='Dionysus for ' + current_user)
+                    # client.send(response)
+                else:
+                    self.clients.remove(client)
+                    raise Exception('Client disconnected')
+            except Exception as e :
+                raise e
+                client.close()
+                return False
+
+    def send_client(self, messege):
+        while(len(self.clients) == 0):
+            time.sleep(0.5)
+        self.clients[-1].send((messege + "\n").encode())
+
+
+
+# if __name__ == '__main__':
+#     # thr = WorkerThread('10.1.3.46', 31285)
+#     server  = ThreadedServer('10.1.3.46', 31285)
+#     server.start()
+#     while(True):
+#         server.send_client("Hi, Mykola!")
+#         time.sleep(2)
+#     # thr.start()
+#     # thr.run()
+#     input()
+#     # setver.stoprequest.
+#     # thr.join()
+#     server.join()
 
 def read_data(file):
   data = pd.read_csv(file, delimiter=',')
@@ -22,8 +152,8 @@ def read_data(file):
               }
       colomns.append(d)
 
-  colomns[10], colomns[2] = colomns[2], colomns[10]
-  colomns[9], colomns[1] = colomns[1], colomns[9]
+  # colomns[10], colomns[2] = colomns[2], colomns[10]
+  # colomns[9], colomns[1] = colomns[1], colomns[9]
 
   # colomns[0].update()
 
@@ -33,15 +163,11 @@ def read_data(file):
         "sortable": False,
         "checkbox" : True
         }
-  colomns[0]["sortable"] = True
+  colomns[0]["sortable"] = False
+  colomns[0]["checkbox"] = True
   # colomns[0]["field"] = "ID"
-  colomns.insert(0, d)
+  # colomns.insert(0, d)
   new_dat =[];
-
-
-
-
-
 
   for j in range(len(data)):
       d = {} 
@@ -54,8 +180,13 @@ def read_data(file):
   return (new_dat, colomns)
 
 # @app.before_request
-# def init_data():
+# def init_data ():
 data, colomns = read_data("../../data/wine.csv")
+print("reading data")
+# @app.after_request
+# def init_server():
+#     #print jdata
+
 
 
 
@@ -66,14 +197,15 @@ data, colomns = read_data("../../data/wine.csv")
 #jdata=json.dumps(data)
 
 @app.route('/' )
-def index():
-
-	# data = request.data
-	# print(data,"\n\n")
-	return render_template("table.html",
-      data=data,
+def index(user = ''):
+  dat  = random.randrange(1, 50)
+  # print(dat)
+  # new_data = data[:dat]
+  new_data = get_wine_data(ids = range(1, dat), data=data, colomns=colomns)
+  return render_template("table.html",
+      data=new_data,
       columns=colomns,
-      title='Dionysus store')
+      title='Dionysus  Jhon')
 
 @app.route('/checked', methods=['GET', 'POST'] )
 def checked():
@@ -89,5 +221,12 @@ def checked():
 	return json.dumps(jdata)
 	
 if __name__ == '__main__':
-	#print jdata
-  app.run(debug=True)
+
+  server  = ThreadedServer('10.1.3.46', 31285)
+  # server.start()
+  print (" * Starting Socket")
+
+  # time.sleep(20)
+  # server.join()
+  app.run(port=5001,debug=False, host='0.0.0.0')
+
--- src/web_ui/communicate.py
+++ src/web_ui/communicate.py
@@ -1,11 +1,30 @@
 import socket
 import threading
-
+from app import data, colomns, index
 import os, time
 import threading, Queue
 
-def get_wine_data(wine_id):
-    
+users = []
+current_user = str()
+previous_user = str()
+def parse_recived_socket(dat):
+
+    dat =  dat.split(" ")
+    user = dat[0]
+    wines = [int(x) for x in dat[1:] ]
+    return user, wines
+
+def get_wine_data(ids):
+    new_dat =[];
+    for j in ids:
+      d = {} 
+      for i in range(1,len(colomns)):
+          d.update( {colomns[i]["field"] : data[colomns[i]["field"]][j] })
+    #         print(d)
+            print(i, j , dat[colomns[i]["field"]][j])
+      new_dat.append(d)
+    return new_dat
+
 
 
 class ThreadedServer(threading.Thread):
@@ -15,8 +34,9 @@ class ThreadedServer(threading.Thread):
         
         self.host = host
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock = socket.socket()
+        # self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
         self.clients = []
         self.stoprequest = threading.Event()
@@ -32,14 +52,25 @@ class ThreadedServer(threading.Thread):
     def listenToClient(self, client, address):
         size = 1024
         self.clients.append(client)
-        while True:
+        while not self.stoprequest.isSet():
             try:
-                data = client.recv(size)
-                if data:
+                msg= client.recv(size)
+                
+                
+
+                if msg:
                     # Set the response to echo back the recieved data 
-                    print(data)
-                    response = data
-                    client.send(response)
+                    print(msg)
+                    current_user, dat = parse_recived_socket(msg)
+                    # response = data
+                    if (current_user not in users):
+                        users.append(current_user)
+                        print(current_user)
+                    else:
+                        if (previous_user != current_user):
+                            data = get_wine_data(dat)
+                            index(current_user)
+                    # client.send(response)
                 else:
                     self.clients.remove(client)
                     raise Exception('Client disconnected')
@@ -55,14 +86,16 @@ class ThreadedServer(threading.Thread):
 
 
 
-if __name__ == '__main__':
-    # thr = WorkerThread('10.1.3.46', 31285)
-    server  = ThreadedServer('10.1.3.46', 31285)
-    server.start()
-    server.send_client("Hi, Mykola!")
-    # thr.start()
-    # thr.run()
-    input()
-    # setver.stoprequest.
-    # thr.join()
-    server.join()
+# if __name__ == '__main__':
+#     # thr = WorkerThread('10.1.3.46', 31285)
+#     server  = ThreadedServer('10.1.3.46', 31285)
+#     server.start()
+#     while(True):
+#         server.send_client("Hi, Mykola!")
+#         time.sleep(2)
+#     # thr.start()
+#     # thr.run()
+#     input()
+#     # setver.stoprequest.
+#     # thr.join()
+#     server.join()
